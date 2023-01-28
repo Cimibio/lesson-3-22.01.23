264cb7a008d23ed97b538a3be7f0a9c3c3ccbd54
AndreySh <shingaryovan@gmail.com>
2023-01-28 17:20:15 +1200

    сортировка массива

---
+++ ex02/Program.cs
@@ -0,0 +1,16 @@
+﻿// сортировка массива
+int[] array = new int[10];
+for (int i = 0; i <= 9; i = i++)
+{
+    array[i] = new Random().Next(0,10);
+}
+void PrintArray(int[] arg)
+{
+    int count = arg.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arg[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
