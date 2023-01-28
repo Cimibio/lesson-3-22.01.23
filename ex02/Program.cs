f9560d3c35ea54c0861a3a845e6ea0fd1b553895
AndreySh <shingaryovan@gmail.com>
2023-01-28 17:36:00 +1200

    сортировка массива методами

--- ex02/Program.cs
+++ ex02/Program.cs
@@ -14,3 +14,21 @@ void PrintArray(int[] arg)
     Console.WriteLine();
 }
 PrintArray(array);
+
+void SelectionSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int minPos = i;
+        for (int j = i +1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[minPos]) minPos = j;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[minPos];
+        arr[minPos] = temp;
+    }
+}
+SelectionSort(array);
+PrintArray(array);
